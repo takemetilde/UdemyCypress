51fce666bd6efa1789028a4d1934b405668f9e44
Hong Her <hongher@Hong-Hers-MacBook-Pro.local>
2020-05-13 09:12:28 -0400

    Inception

--- cypress/integration/test/features/test/test.spec.js
+++ cypress/integration/test/features/test/test.spec.js
@@ -1,10 +1,10 @@
 /// <reference types = "Cypress" />
 
-import {Given, And, Then, When} from "cypress-cucumber-preprocessor/steps"
+import {Given, When} from "cypress-cucumber-preprocessor/steps"
 
 import TravelPage from "../../pages/travelpage"
 
-var travelPage = new TravelPage();
+const travelPage = new TravelPage()
 
 Given('This is the first', () => {
     cy.visit(travelPage.baseUrl);
